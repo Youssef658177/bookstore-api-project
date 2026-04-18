e6a92bb2e700754d9300eda1b081c9910627c270
Youssef658177 <josephsalex5@gmail.com>
2026-04-18 21:36:21 +0200

    Initialize books database with sample entries

---
+++ booksdb.js
@@ -0,0 +1,8 @@
+let books = {
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "isbn": "12345" },
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "isbn": "67890" },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "isbn": "11223" },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "isbn": "44556" }
+};
+
+module.exports = books;
